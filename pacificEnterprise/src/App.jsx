a6dbc830066502c3333859c710c600ae8a6fd15a
NatSeg11 <natalia.segnini@ucr.ac.cr>
2024-09-09 18:08:23 -0600

    Prueba

--- pacificEnterprise/src/App.jsx
+++ pacificEnterprise/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './Components/ui/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div>
         <h1 class="text-3xl font-bold underline">Hola</h1>
+        <Card />
+      
       </div>
     </>
   )
---
+++ pacificEnterprise/src/Components/ui/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import "../../index.css";
+
+
+
+export function Card() {
+    return (
+     <>
+        <div className='bg-[#5782e8]' >
+  <h1>Hola</h1>
+        </div>
+     </>
+    )
+}
+
+export default Card
