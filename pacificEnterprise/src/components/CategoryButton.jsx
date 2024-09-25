123a470ca9b930cf60de91bf9bcc255a10be8548
NatSeg11 <natalia.segnini@ucr.ac.cr>
2024-09-24 21:52:45 -0600

    Estilos en los botones

--- pacificEnterprise/src/components/CategoryButton.jsx
+++ pacificEnterprise/src/components/CategoryButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 const CategoryButton = ({ id, name, onCategoryClick }) => {
     return (
-        <button onClick={() => onCategoryClick(id)}>
+        <button className='border-4 text-xl py-[1vh] px-[5vw] rounded-[2vw] width-full hover:bg-[#f5f5f5] hover:border-[#16161A] hover:text-[#16161A] ' onClick={() => onCategoryClick(id)}>
             {name}
         </button>
     );
