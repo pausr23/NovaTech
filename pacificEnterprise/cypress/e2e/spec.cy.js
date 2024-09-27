f02179f05a620fed049a8609dbbf9f744dd77275
KarolCastroCh <karolcastro174@gmail.com>
2024-09-26 23:12:18 -0600

    Cypress

--- pacificEnterprise/cypress/e2e/spec.cy.js
+++ pacificEnterprise/cypress/e2e/spec.cy.js
@@ -1,8 +1,28 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Pruebas de Landing Page', () => {
+  it('Botón Menu', () => {
     cy.visit('http://localhost:5173/')
 
-    cy.contains('button').click()
-
+    cy.get('button').contains('Menu').click()
+    
   })
+
+  it('Pruebas de navbar', () => {
+    cy.visit('http://localhost:5173/')
+
+    cy.get('li').contains('Menu').click()
+    cy.get('li').contains('About us').click()
+    cy.get('li').contains('Home').click()
+  })
+
+  it('Pruebas de Carrusel', () => {
+    cy.visit('http://localhost:5173/')
+
+    cy.get('img[alt="flecha go"]').click()
+    cy.get('img[alt="flecha back"]').click()
+    cy.get('img[alt="flecha go"]').click()
+    cy.get('img[alt="flecha back"]').click()
+    cy.get('img[alt="flecha go"]').click()
+    cy.get('img[alt="flecha back"]').click()
+  })
+
 })
