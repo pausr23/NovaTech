d2779ccb4dc3eb93fe3063cd1c4eb220ec9d4c2f
KarolCastroCh <karolcastro174@gmail.com>
2024-09-26 23:44:19 -0600

    prueba de menu

--- pacificEnterprise/cypress/e2e/spec.cy.js
+++ pacificEnterprise/cypress/e2e/spec.cy.js
@@ -24,5 +24,36 @@ describe('Pruebas de Landing Page', () => {
     cy.get('img[alt="flecha go"]').click()
     cy.get('img[alt="flecha back"]').click()
   })
+})
+
+  describe('Pruebas de About Us', () => {
+
+    it('Pruebas de scroll', () => {
+      cy.visit('http://localhost:5173/about-us')
+  
+      cy.get('div.grid-cols-2.h-full')
+      .should('be.visible');
+  
+    cy.scrollTo('bottom'); 
+  
+    cy.get('div.h-60') 
+      .should('be.visible');
+    })
+
+  })
+
+    describe('Pruebas de Menu', () => {
+
+      it('Scroll de menú', () => {
+        cy.visit('http://localhost:5173/menu')
+    
+        cy.get('div.text-white h1')
+        .should('be.visible');
+    
+      cy.scrollTo('bottom'); 
+    
+      cy.get('div.h-60') 
+        .should('be.visible');
+      })
 
 })
